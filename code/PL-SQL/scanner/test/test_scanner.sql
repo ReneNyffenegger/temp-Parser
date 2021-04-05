c5a24211960a7317664de47759376866d956d8f5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-04-05 12:31:26 +0200

    n/a

--- code/PL-SQL/scanner/test/test_scanner.sql
+++ code/PL-SQL/scanner/test/test_scanner.sql
@@ -10,6 +10,18 @@ begin
 fo0
 bar }';
 
+  prog := prog                  ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*') ||
+          rpad('*', 10000, '*');
+
   scanner := new scanner_clob(prog);
 
   scanner.next_char; if scanner.current_character_ <> '.'      then dbms_output.put_line('nok 1'); dbms_output.put_line(scanner.current_character_); end if;
@@ -21,7 +33,25 @@ bar }';
   scanner.next_char; if scanner.current_character_ <> 'b'      then dbms_output.put_line('nok b'); end if;
   scanner.next_char; if scanner.current_character_ <> 'a'      then dbms_output.put_line('nok a'); end if;
   scanner.next_char; if scanner.current_character_ <> 'r'      then dbms_output.put_line('nok r'); end if;
-  scanner.next_char; if scanner.current_character_ <> ' '      then dbms_output.put_line('nok'); end if;
+  scanner.next_char; if scanner.current_character_ <> ' '      then dbms_output.put_line('nok'  ); end if;
+
+  for i in 1 .. 1e5 loop
+    begin
+
+      scanner.next_char;
+      if scanner.current_character_ <> '*' then dbms_output.put_line('*'); end if;
+
+    exception when others then
+      dbms_output.put_line('i = ' || i);
+      raise;
+    end;
+  end loop;
+
+  dbms_output.put_line('reading past last character');
+
   scanner.next_char; if scanner.current_character_ is not null then dbms_output.put_line('nok null'); end if;
+
+  dbms_output.put_line('finished');
+
 end;
 /
