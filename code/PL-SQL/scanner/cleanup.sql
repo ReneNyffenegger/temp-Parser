cc22c702475ee50e2c20b722eef6c783b6287140
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-04-05 12:20:06 +0200

    n/a

--- code/PL-SQL/scanner/cleanup.sql
+++ code/PL-SQL/scanner/cleanup.sql
@@ -1,4 +1,3 @@
 drop type scanner_all_source;
 drop type scanner_varchar2;
 drop type scanner;
-
--- code/PL-SQL/scanner/test/test_scanner.sql
+++ code/PL-SQL/scanner/test/test_scanner.sql
@@ -1,22 +1,27 @@
 declare
-  vc2_scanner scanner_varchar2 := new scanner_varchar2(
-q'{.
-fo0
-bar }');
+
+  prog clob;
+  scanner scanner_clob;
+
 
 begin
 
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> '.'      then dbms_output.put_line('nok 1'); dbms_output.put_line(vc2_scanner.current_character_); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> chr(10)  then dbms_output.put_line('nok 2'); dbms_output.put_line(vc2_scanner.current_character_); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> 'f'      then dbms_output.put_line('nok f'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> 'o'      then dbms_output.put_line('nok o'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> '0'      then dbms_output.put_line('nok 0'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> chr(10)  then dbms_output.put_line('nok 6'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> 'b'      then dbms_output.put_line('nok b'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> 'a'      then dbms_output.put_line('nok a'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> 'r'      then dbms_output.put_line('nok r'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ <> ' '      then dbms_output.put_line('nok'); end if;
-  vc2_scanner.next_char; if vc2_scanner.current_character_ is not null then dbms_output.put_line('nok null'); end if;
+  prog := q'{.
+fo0
+bar }';
+
+  scanner := new scanner_clob(prog);
+
+  scanner.next_char; if scanner.current_character_ <> '.'      then dbms_output.put_line('nok 1'); dbms_output.put_line(scanner.current_character_); end if;
+  scanner.next_char; if scanner.current_character_ <> chr(10)  then dbms_output.put_line('nok 2'); dbms_output.put_line(scanner.current_character_); end if;
+  scanner.next_char; if scanner.current_character_ <> 'f'      then dbms_output.put_line('nok f'); end if;
+  scanner.next_char; if scanner.current_character_ <> 'o'      then dbms_output.put_line('nok o'); end if;
+  scanner.next_char; if scanner.current_character_ <> '0'      then dbms_output.put_line('nok 0'); end if;
+  scanner.next_char; if scanner.current_character_ <> chr(10)  then dbms_output.put_line('nok 6'); end if;
+  scanner.next_char; if scanner.current_character_ <> 'b'      then dbms_output.put_line('nok b'); end if;
+  scanner.next_char; if scanner.current_character_ <> 'a'      then dbms_output.put_line('nok a'); end if;
+  scanner.next_char; if scanner.current_character_ <> 'r'      then dbms_output.put_line('nok r'); end if;
+  scanner.next_char; if scanner.current_character_ <> ' '      then dbms_output.put_line('nok'); end if;
+  scanner.next_char; if scanner.current_character_ is not null then dbms_output.put_line('nok null'); end if;
 end;
 /
-
